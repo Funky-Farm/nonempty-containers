bc202b8977683f3834ff4d664df378b04ddb6ade
Lancelot Liu <me@lancy.dev>
2025-03-08 12:29:59 +0000

    fix: `Arbitrary` implementation for `NonEmptySet`.

--- src/arbitrary.rs
+++ src/arbitrary.rs
@@ -1,12 +1,24 @@
 //! Implementations for [Arbitrary] for non-empty container types.
 
-use crate::NonEmptyVec;
+use std::collections::HashSet;
+use std::hash::Hash;
+use crate::{NonEmptySet, NonEmptyVec};
 use arbitrary::{Arbitrary, Unstructured};
 
 impl<'a, T: Arbitrary<'a>> Arbitrary<'a> for NonEmptyVec<T> {
     fn arbitrary(u: &mut Unstructured<'a>) -> arbitrary::Result<Self> {
         let head = T::arbitrary(u)?;
-        let tail = Vec::<T>::arbitrary(u)?;
-        Ok(Self::new(head, tail))
+        let mut tail = Vec::<T>::arbitrary(u)?;
+        tail.push(head);
+        Ok(Self::__from_vec_unsafe(tail))
+    }
+}
+
+impl<'a, T: Arbitrary<'a> + Eq + Hash> Arbitrary<'a> for NonEmptySet<T> {
+    fn arbitrary(u: &mut Unstructured<'a>) -> arbitrary::Result<Self> {
+        let head = T::arbitrary(u)?;
+        let mut tail = HashSet::<T>::arbitrary(u)?;
+        tail.insert(head);
+        Ok(Self::__from_set_unsafe(tail))
     }
 }
